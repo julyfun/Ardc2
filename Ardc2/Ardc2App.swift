a1078d560f19460d8071c0d621b1625f5ae6666d
Julyfun MacOS14.5 M1 <43675484+julyfun@users.noreply.github.com>
2025-02-25 22:50:36 +0800

    Initial Commit

---
+++ Ardc2/Ardc2App.swift
@@ -0,0 +1,17 @@
+//
+//  Ardc2App.swift
+//  Ardc2
+//
+//  Created by florian on 2025/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Ardc2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ardc2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ardc2
+//
+//  Created by florian on 2025/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
